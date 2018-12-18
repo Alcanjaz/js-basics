091431302754393ec6ef70266cd262af1e4568e8
Alcanjaz <alexiscaposch15@gmail.com>
2018-12-18 02:52:23 -0300

    added input reading

--- budget-project/app.js
+++ budget-project/app.js
@@ -1,29 +1,63 @@
+
+//BUDGET CONTROLLER
 let budgetController = (function() {
-    let x = 23;
-
-    let add = function(a){
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            console.log(add(b));
-        }
-    }
+    
 })();
 
+//UI CONTROLLER
 let UIController = (function() {
 
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription:'.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput() {
+            return{
+            type: document.querySelector(DOMstrings.inputType).value,//will be either inc o exp
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings() {
+            return DOMstrings;
+        }
+    };
+
 })();
 
+//GLOBAL APP CONTROLLER
 let controller = (function(budgetCtrl, UICtrl) {
 
-    let test = budgetCtrl.publicTest(5);
+    let DOM = UICtrl.getDOMstrings();
 
-    return{
-        anotherPublic: function(){
-            console.log(test);
-        }
+    let ctrlAddItem = function () {
+
+        //1.Get the field input data
+        let input = UICtrl.getInput();
+        console.log(input);
+        
+        //2.Add the item to the budget controller
+
+        //3.Add the item to the UI
+
+        //4.Calculate the budget
+
+        //5.Display the budget to the UI
+
+         
+        console.log('it works');
     }
 
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(e) {
+        if (event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
+    
 })(budgetController, UIController);
